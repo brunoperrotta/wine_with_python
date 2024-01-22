2cd738a0985741a8080ef45cad40ebcf2da05685
brunoperrotta <146884970+brunoperrotta@users.noreply.github.com>
2024-01-22 11:28:48 -0300

    Update wine_python.py

--- wine_python.py
+++ wine_python.py
@@ -76,7 +76,7 @@ fig_top5 = px.scatter(filtro_ano_top5, x='Ano', y='Valor(US$)',
                      'Chile': 'darkred',      
                      'Argentina': 'lightblue', 
                      'Portugal': 'darkgreen',  
-                     'Italia': 'blue',      
+                     'Itália': 'blue',      
                      'França': 'pink'        
                  },
                   range_x=[1999, 2023], range_y=[0, 250000000])
