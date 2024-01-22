870ff145b711bc3a6d8aed86126b6ae0b9677ead
brunoperrotta <146884970+brunoperrotta@users.noreply.github.com>
2024-01-22 11:35:06 -0300

    Update wine_python.py

--- wine_python.py
+++ wine_python.py
@@ -70,8 +70,8 @@ with row1[1]:
 filtro_ano_top5 = top5[(top5['Year'] >= 1999) & (top5['Year'] <= 2022)]
 
 fig_top5 = px.scatter(filtro_ano_top5, x='Year', y='Valor(US$)',
-                  size=200, color='País',
-                 hover_name='País', log_x=True, size_max=200, animation_frame='Year',
+                  size='Valor(US$)', color='País',
+                 hover_name='País', log_x=True, animation_frame='Year',
                   color_discrete_map={
                      'Chile': 'darkred',      
                      'Argentina': 'lightblue', 
